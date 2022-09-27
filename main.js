84564a5e486f0f4a4b15452f49d3e19d4e9a0a24
Jesús Paredes <jjesus.paredesl@gmail.com>
2022-09-27 19:31:17 -0300

    entrega

---
+++ main.js
@@ -0,0 +1,101 @@
+let appMusica = prompt('Desea Abrir App de Musica');
+const cerrarApp = ()=>
+alert('Cerrando la app...')
+
+
+let listaPlaylist = [
+
+  {
+      Grupo: 'The Beatles',
+      Cancion: 'Let it be'
+  },
+  {
+      Grupo: 'Rolling Stones',
+      Cancion: 'Angie'
+  },
+  {
+      Grupo: 'John Lennon',
+      Cancion: 'Imagine'
+  },
+  {
+      Grupo: 'Led Zeppelin',
+      Cancion: 'Stairway to heaven'
+  },
+  {
+      Grupo: 'The Doors',
+      Cancion: 'The End'
+  },
+  {
+      Grupo: 'Rod Stewart',
+      Cancion: 'Young Turks'
+  },
+
+]
+
+if (appMusica === 'Si' || appMusica === 'si' || appMusica === 'YES' || appMusica === 'yes') {
+  abriendoApp()
+}
+else {
+alert('Cerrando app ...')
+}
+
+//------------------------------------------------------------------------
+
+
+function abriendoApp() {
+
+  let peticionUsuario = prompt('Ingrese nombre de usuario');
+  let peticionContrasenia = prompt('Ingrese contrasenia');
+  const contraseniaVerificada = '1234';
+  const usuarioVerificado = 'Jesus'
+
+
+if( peticionUsuario === usuarioVerificado && peticionContrasenia ===contraseniaVerificada) {
+  appBienvenida()
+}
+else{
+  cerrarApp()
+}}
+
+ // ------------------------------------------------------------------------------------
+
+
+function appBienvenida() {
+
+  alert(`Bienvenido... cargando playlist`)
+  cargandoPlaylist()
+
+}
+
+
+function cargandoPlaylist() {
+
+  for (let i = 1; i < listaPlaylist.length; i++) {
+
+    console.log(listaPlaylist[i]);
+
+    } 
+
+
+}
+   const elegirTema= prompt('Elija un numero de tema (1,2,3,4,5)')
+   const temaUno = '1';
+   const temaDos ='2';
+   const temaTres = '3';
+   const temaCuatro = '4';
+   const temaCinco = '5';
+
+   if (elegirTema === temaUno|| elegirTema === temaDos|| elegirTema === temaTres|| elegirTema ===temaCuatro|| elegirTema === temaCinco) {
+
+    console.log('reproduciendo su tema...')
+    
+    } 
+    
+    else {
+    
+    console.log(cerrarApp())
+    
+    }
+
+
+
