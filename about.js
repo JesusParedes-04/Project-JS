93a434a95b99ca9635fad3fe8b1a49f89ef946d5
Jesús Paredes <jjesus.paredesl@gmail.com>
2023-01-09 19:40:17 -0300

    cambio imagenes

--- about.js
+++ about.js
@@ -9,18 +9,24 @@ fetch(perfilMusica)
 	.then((respuesta) => respuesta.json())
 	.then((data) => {
 
-		data.forEach((datainfo) =>{
-contenedorDatos2.innerHTML += `
+		data.forEach((datainfo) => {
+			contenedorDatos2.innerHTML += `
 <h2 style="color:white"> ${datainfo.nombre} </h2>
-<img src="${datainfo.img} " alt="">
-<p> ${datainfo.descripcion} </p>
+<div class= "container-fluid">
+<div class="row">
+<div class = "col-sm-1 col-md-5 col-lg-6">
+<img class="img-fluid" src="${datainfo.img} " alt=""> </div> 
+<div class = "col-sm-1 col-md-5 col-lg-6">
+<p class="d-flex align-items-center"> ${datainfo.descripcion} </p> <div/> 
+</div> 
+</div> 
 `
 
-	})
+		})
 
 	})
 
-.catch(error => console.error((error)));
+	.catch(error => console.error((error)));
 
 
 
